7c43927bc2a07fcc922a25b535a78edf18177755
David Hartman <djhartman1216@gmail.com>
2026-02-19 13:46:16 -0500

    Sync codebases

--- scenes/spells/magic missile/magic missile.gd
+++ scenes/spells/magic missile/magic missile.gd
@@ -15,33 +15,41 @@ func _ready() -> void:
 func _network_spawn(data : Dictionary) -> void:
 	parent_nodepath = data["spellslot_path"]
 	direction = data['tile_position'] - data['cast_position']
-	global_position = (Vector2(data['cast_position'] * 16) + Vector2(8, 8)) + direction
-	spell_cast.emit.call_deferred()
+	#global_position = (Vector2(data['cast_position'] * 16) + Vector2(8, 8)) + direction
+	global_position = data['tile_position'] * 16 + Vector2(8,8)
+	#spell_cast.emit.call_deferred()
 	active = true
+	print(global_position)
+	print(get_parent().name)
+	print(visible)
 
 func _network_process(_input: Dictionary) -> void:
 	if game.current_map.query_location(global_position):
 		SyncManager.despawn(self)
 		return
 	
-	global_position += direction.normalized() * speed
+	#global_position += direction.normalized() * speed
 
 func _on_SyncManager_scene_spawned(_name, spawned_node, _scene, _data) -> void:
 	if spawned_node == self and not active:
+		return
 		spell_cast.connect(get_node(parent_nodepath).reset_spell_slot)
 
 func _on_SyncManager_scene_despawned(_name, spawned_node) -> void:
 	if spawned_node == self and not active:
+		return
 		spell_cast.disconnect(get_node(parent_nodepath).reset_spell_slot)
 
 func _save_state() -> Dictionary:
 	return {
+		parent_nodepath = parent_nodepath,
 		global_position = global_position,
 		direction = direction,
 		active = active
 	}
 
 func _load_state(state : Dictionary) -> void:
+	parent_nodepath = state['parent_nodepath']
 	global_position = state['global_position']
 	direction = state['direction']
 	active = state['active']
