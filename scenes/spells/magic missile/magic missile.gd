9b703b1fcf80d89d49e3f902da43e2be20467538
David Hartman <djhartman1216@gmail.com>
2026-02-19 15:05:54 -0500

    Another test

--- scenes/spells/magic missile/magic missile.gd
+++ scenes/spells/magic missile/magic missile.gd
@@ -14,11 +14,11 @@ func _ready() -> void:
 
 func _network_spawn(data : Dictionary) -> void:
 	parent_nodepath = data["spellslot_path"]
-	direction = data['tile_position'] - data['cast_position']
+	#direction = data['tile_position'] - data['cast_position']
 	#global_position = (Vector2(data['cast_position'] * 16) + Vector2(8, 8)) + direction
 	global_position = data['tile_position'] * 16 + Vector2(8,8)
 	#spell_cast.emit.call_deferred()
-	active = true
+	#active = true
 
 func _network_process(_input: Dictionary) -> void:
 	if game.current_map.query_location(global_position):
@@ -39,14 +39,14 @@ func _on_SyncManager_scene_despawned(_name, spawned_node) -> void:
 
 func _save_state() -> Dictionary:
 	return {
-		parent_nodepath = parent_nodepath,
+		#parent_nodepath = parent_nodepath,
 		global_position = global_position,
-		direction = direction,
+		#direction = direction,
 		active = active
 	}
 
 func _load_state(state : Dictionary) -> void:
-	parent_nodepath = state['parent_nodepath']
+	#parent_nodepath = state['parent_nodepath']
 	global_position = state['global_position']
-	direction = state['direction']
+	#direction = state['direction']
 	active = state['active']
