61f1ffc37bc7a5186333637212bfb0169b3d08e5
David Hartman <djhartman1216@gmail.com>
2026-02-20 15:16:22 -0500

    Can't see spells you didn't cast.

--- scenes/effects/fire/fire.gd
+++ scenes/effects/fire/fire.gd
@@ -18,6 +18,28 @@ func check_damage_players() -> void:
 			player.decrement_health()
 			return
 
+
+func _process(_delta : float) -> void:
+	set_visibility()
+
+func set_visibility() -> void:
+	var my_peer_id : int
+	if multiplayer.is_server(): my_peer_id = 1
+	else: my_peer_id = multiplayer.get_unique_id()
+	
+	var space_state = get_world_2d().direct_space_state
+	var position_to_check : Vector2 = Vector2.ZERO
+	for _player in get_tree().get_nodes_in_group("players"):
+		if _player.get_multiplayer_authority() == my_peer_id:
+			position_to_check = _player.global_position
+	
+	var query = PhysicsRayQueryParameters2D.create(global_position, position_to_check)
+	var result = space_state.intersect_ray(query)
+	if result:
+		visible = false
+		return
+	visible = true
+
 func increment_ticks() -> void:
 	check_damage_players()
 	current_ticks += 1
--- scenes/spells/mark and recall/mark_and_recall.gd
+++ scenes/spells/mark and recall/mark_and_recall.gd
@@ -18,12 +18,33 @@ func _network_spawn(data : Dictionary) -> void:
 		spell_cast.connect(get_node(parent_nodepath).reset_spell_slot)
 		active = true
 
+func _process(_delta : float) -> void:
+	set_visibility()
+
+func set_visibility() -> void:
+	var my_peer_id : int
+	if multiplayer.is_server(): my_peer_id = 1
+	else: my_peer_id = multiplayer.get_unique_id()
+	
+	var space_state = get_world_2d().direct_space_state
+	var position_to_check : Vector2 = Vector2.ZERO
+	for _player in get_tree().get_nodes_in_group("players"):
+		if _player.get_multiplayer_authority() == my_peer_id:
+			position_to_check = _player.global_position
+	
+	var query = PhysicsRayQueryParameters2D.create(global_position, position_to_check)
+	var result = space_state.intersect_ray(query)
+	if result:
+		visible = false
+		return
+	visible = true
+
 func _network_process(_input: Dictionary) -> void:
 	if game.current_map.query_location(global_position):
 		SyncManager.despawn(self)
 		return
 
-func receive_input(data : Dictionary) -> void:
+func receive_input(_data : Dictionary) -> void:
 	player.global_position = global_position
 	spell_cast.emit()
 	SyncManager.despawn(self)
